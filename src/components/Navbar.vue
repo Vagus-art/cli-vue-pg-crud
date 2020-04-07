cec68ac4397f18a0a7e49d5a50bb4801a7ab597d
Vagus-art <agustin.favo@gmail.com>
2020-04-07 15:48:48 -0300

    basic structure done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Vue-Components</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            >Apis</a
+          >
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">pg-rest-api</a>
+            <a class="dropdown-item" href="#">clean-pg-rest-api</a>
+            <a class="dropdown-item" href="#">php-rest-api</a>
+            <a class="dropdown-item" href="#">clean-php-rest-api</a>
+          </div>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import "bootstrap";
+
+@Component
+export default class Navbar extends Vue {}
+</script>
+
